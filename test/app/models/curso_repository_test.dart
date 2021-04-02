b230ec0ebe6d7533447379640f565b5ad6e31a3b
Egito <egito.salvador@gmail.com>
2021-04-02 17:54:50 -0300

    Ajuste em definicao de variavel

--- test/app/models/curso_repository_test.dart
+++ test/app/models/curso_repository_test.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flu_acessos/app/models/curso_repository.dart';
- 
+
 void main() {
-  late CursoRepository repository;
+  final CursoRepository repository;
 
   setUpAll(() {
     repository = CursoRepository();
